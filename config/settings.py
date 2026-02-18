cb9e65a3733e4a77b67a98866fc853bf4728261e
axrorback <axacyber1@gmail.com>
2026-02-18 11:31:18 +0500

    Update `ALLOWED_HOSTS` to include deployment domains

--- config/settings.py
+++ config/settings.py
@@ -9,7 +9,7 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 import dj_database_url
 DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['student.axror.tech', '127.0.0.1', 'localhost',]
 
 INSTALLED_APPS = [
     'django.contrib.admin',
