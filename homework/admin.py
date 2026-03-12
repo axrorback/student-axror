f00911944a79586c747de29d789b87f7b44aac9d
axrorback <axrorback@gmail.com>
2026-03-12 07:01:17 +0500

    Register Assignment and AssignmentSubmission models in the admin panel with custom list displays

--- homework/admin.py
+++ homework/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from .models import Assignment , AssignmentSubmission
 
-# Register your models here.
+@admin.register(Assignment)
+class AssignmentAdmin(admin.ModelAdmin):
+    list_display = ['lesson','title','status']
+
+
+@admin.register(AssignmentSubmission)
+class AssignmentSubmissionAdmin(admin.ModelAdmin):
+    list_display = ['assignment','status','student']
