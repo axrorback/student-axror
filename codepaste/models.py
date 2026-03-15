aad7a99d5c82d7fb01b6eec7211ef0febac42042
axrorback <axrorback@gmail.com>
2026-03-16 02:25:08 +0500

    Add `CodePaste` model with language choices, expiration logic, and user association

--- codepaste/models.py
+++ codepaste/models.py
@@ -1,3 +1,21 @@
 from django.db import models
+from django.utils import timezone
 
-# Create your models here.
+
+class CodePaste(models.Model):
+
+    LANGUAGE_CHOICES = [
+        ("python", "Python"),
+        ("html", "HTML"),
+        ("shell", "Shell"),
+    ]
+
+    user = models.ForeignKey('accounts.StudentProfile', on_delete=models.CASCADE)
+    language = models.CharField(max_length=20, choices=LANGUAGE_CHOICES)
+    code = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    expire_at = models.DateTimeField()
+    is_active = models.BooleanField(default=True)
+
+    def is_expired(self):
+        return timezone.now() >= self.expire_at
