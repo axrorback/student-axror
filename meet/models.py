6d6f3e667eb2af2d86114475c988a7aedee19e47
axrorback <axrorback@gmail.com>
2026-06-08 13:36:04 +0500

    Add `Meeting` model to `meet` app

--- meet/models.py
+++ meet/models.py
@@ -1,3 +1,13 @@
+import uuid
 from django.db import models
+from lessons.models import Lesson
 
-# Create your models here.
+class Meeting(models.Model):
+    id = models.UUIDField(primary_key=True,default=uuid.uuid4,editable=False)
+    lesson = models.OneToOneField(Lesson,on_delete=models.CASCADE,related_name="meeting")
+    room_name = models.CharField(max_length=255,unique=True)
+    is_active = models.BooleanField(default=False)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.room_name
