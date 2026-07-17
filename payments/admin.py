c8d7336dec788d85f6ce483aa53335e2e3749cd9
axrorback <axrorback@gmail.com>
2026-07-17 19:40:34 +0500

    Register `Course` and `Order` models in admin panel with custom admin classes

--- payments/admin.py
+++ payments/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Course , Order
+
+
+class CourseAdmin(admin.ModelAdmin):
+    list_display = ('title','slug','price','description')
+
+
+class OrderAdmin(admin.ModelAdmin):
+    list_display = ('id','status','amount','course',)
+
+
+admin.site.register(Course,CourseAdmin)
+admin.site.register(Order,OrderAdmin)
+
+
