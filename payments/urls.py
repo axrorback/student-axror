6ee98ef9d4dc7a91e343f06da358be5ed2e3b421
axrorback <axrorback@gmail.com>
2026-07-17 19:13:33 +0500

    Add URL routes for `payments` app

---
+++ payments/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+
+from .views import *
+
+urlpatterns = [
+    path('courses/',courses,name='courses'),
+    path('pay/',pay_course,name='pay_course'),
+    path('click/callback/',payment_callback,name='payment_callback'),
+    path('payment/status/<uuid:uuid>/',payment_success,name='payment_success'),
+]
