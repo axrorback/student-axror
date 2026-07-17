8afc734b41b22a6fd9e081bae89c1dea6d085792
axrorback <axrorback@gmail.com>
2026-07-17 20:17:21 +0500

    Update `pay_course` URL to include UUID parameter

--- payments/urls.py
+++ payments/urls.py
@@ -4,7 +4,7 @@ from .views import *
 
 urlpatterns = [
     path('courses/',courses,name='courses'),
-    path('pay/',pay_course,name='pay_course'),
+    path('pay/<uuid:uuid>/',pay_course,name='pay_course'),
     path('click/callback/',payment_callback,name='payment_callback'),
     path('payment/status/<uuid:uuid>/',payment_success,name='payment_success'),
 ]
